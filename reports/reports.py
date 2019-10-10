06d5c2c2292c7e9cb561bb7f214fa9037bb633dd
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2019-10-10 16:23:51 -0400

    Update reports.py

--- reports/reports.py
+++ reports/reports.py
@@ -6,72 +6,68 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-class Report(commands.Cog): 
-    """An easy way for your members to report bad behavior"""
+class Appeal(commands.Cog): 
+    """Appeal system"""
     
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
 
-    @commands.command(aliases=["rchannel"])
+    @commands.command(aliases=["achannel"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def reportchannel(self, ctx, channel: discord.TextChannel):
-        """Set the Reports Channel"""
+        """Set the Appeal Channel"""
         await self.db.find_one_and_update(
-                {"_id": "config"}, {"$set": {"report_channel": channel.id}}, upsert=True
+                {"_id": "config"}, {"$set": {"appeal_channel": channel.id}}, upsert=True
             )
         embed = discord.Embed(
                 color=discord.Color.blue())
         embed.timestamp = datetime.datetime.utcnow()
         embed.add_field(
-            name="Set Channel", value=f"Successfully Set the Report Channel to {channel.mention}",inline=False
+            name="Set Channel", value=f"Successfully Set the Appeal Channel to {channel.mention}",inline=False
         )
         await ctx.send(embed=embed)
 
-    @commands.command(aliases=["rmention"])
+    @commands.command(aliases=["amention"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def reportmention(self, ctx, *, mention: str):
-        """Sets the Report Mention"""
+    async def appealmention(self, ctx, *, mention: str):
+        """Sets the Appeal Mention"""
         await self.db.find_one_and_update(
-                {"_id": "config"}, {"$set": {"report_mention": mention}}, upsert=True
+                {"_id": "config"}, {"$set": {"appeal_mention": mention}}, upsert=True
             )
         embed = discord.Embed(
                 color=discord.Color.blue())
         embed.timestamp = datetime.datetime.utcnow()
         embed.add_field(
-            name="Changed Mention", value=f"Successfully Changed the Report Mention to {mention}",inline=False
+            name="Changed Mention", value=f"Successfully Changed the appeal mention to {mention}",inline=False
         )
         await ctx.send(embed=embed)
         
 
 
     @commands.command()
-    async def report(self, ctx, user: discord.Member, *, reason):
-        """Report member's bad behavior"""
+    async def appeal(self, ctx, user: discord.Member, *, reason):
+        """appeal blacklist"""
         config = await self.db.find_one({"_id": "config"})
-        report_channel = config["report_channel"]
-        setchannel = discord.utils.get(ctx.guild.channels, id=int(report_channel))
+        report_channel = config["appeal_channel"]
+        setchannel = discord.utils.get(ctx.guild.channels, id=int(appeal_channel))
         try:
-            report_mention = config["report_mention"]
-            await setchannel.send(report_mention)
+            report_mention = config["appeal_mention"]
+            await setchannel.send(appeal_mention)
         except:
             pass
 
         embed = discord.Embed(
                     color=discord.Color.red())
         embed.timestamp = datetime.datetime.utcnow()
-        embed.set_author(name=ctx.author.name,icon_url=ctx.author.avatar_url)
-        embed.add_field(
-                name="Reported User", value=f"{user.mention} | ID: {user.id}",inline=False)
+        embed.set_author("Appeal Blacklist system")
         embed.add_field(
                 name="Reported By:", value=f"{ctx.author.mention} | ID: {ctx.author.id}",inline=False)
-        embed.add_field(
-                name="Channel", value=ctx.channel.mention,inline=False)
         embed.add_field(
                 name="Reason", value=reason,inline=False)
 
         await setchannel.send(embed=embed)
-        await ctx.send("Succesfully Reported the User!")
+        await ctx.send("Your appeal is now pending!")
                         
 def setup(bot):
     bot.add_cog(Report(bot))

4d3c15ebb546d0448a641929b2e79a07c7cb194a
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-14 11:53:09 -0400

    Update kickpending.py

--- kickpending/kickpending.py
+++ kickpending/kickpending.py
@@ -17,39 +17,6 @@ def banpending(sc):
         role = ctx.guild.get_role(754859445766586419)
         for x in role.members:
              x.ban(reason="Enemy")
-            await ctx.send("Kicked all members with role Pending")
-        
-        config = await self.db.find_one({"_id": "config"})
-
-        if config is None:
-            return await ctx.send("There's no configured log channel.")
-        else:
-            channel = ctx.guild.get_channel(int(config["channel"]))
-
-        if channel is None:
-            await ctx.send("There is no configured log channel.")
-            return
-
-        try:
-            for member in members:
-                await member.kick(reason=f"{reason if reason else None}")
-                embed = discord.Embed(
-                    color=discord.Color.red(),
-                    title=f"{member} was kicked!",
-                    timestamp=datetime.datetime.utcnow(),
-                )
-
-                embed.add_field(
-                    name="Moderator", value=f"Judge Anthony", inline=False,
-                )
-
-                if reason is not None:
-                    embed.add_field(name="Reason", value=reason, inline=False)
-
-                    await ctx.send(f" {member} is now ban!")
-                    await channel.send(embed=embed)
-        except:
-            return
 s.enter(60, 1, banpending, (s,))
 s.run()
    

2190b98ce2adec22c715cf04b9b6afa5770cdbf4
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-24 18:32:45 -0400

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -6,16 +6,14 @@ import asyncio
 class ChangeStatus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.first = "Test"
-        self.second = "Test"
-        self.third = "Test"
+ 
 
     @tasks.loop(seconds=10)
-        await self.bot.change_presence(activity=discord.Game(name=f"{self.first}"))
+        await self.bot.change_presence(activity=discord.Game(name=f"Test"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name=f"{self.second}"))
+        await self.bot.change_presence(activity=discord.Game(name=f"Test"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name=f"{self.third}"))
+        await self.bot.change_presence(activity=discord.Game(name=f"Test"))
         await asyncio.sleep(10)
 
 def setup(bot):

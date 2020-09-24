cfc12ae4343c5a69a5a8e5a7d2577d5d694a91c9
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-24 18:30:44 -0400

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -1,1 +1,22 @@
 
+import discord
+from discord.ext import commands, tasks
+import asyncio
+
+class ChangeStatus(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.first = None
+        self.second = None
+        self.third = None
+
+    @tasks.loop(seconds=10)
+        await self.bot.change_presence(activity=discord.Game(name=f"{self.first}"))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name=f"{self.second}"))
+        await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Game(name=f"{self.third}"))
+        await asyncio.sleep(10)
+
+def setup(bot):
+    bot.add_cog(ChangeStatus(bot))

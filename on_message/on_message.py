18ef2acbf13d6d0901e048710f07c79a9198f8c7
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-25 13:29:12 -0400

    Update on_message.py

--- on_message/on_message.py
+++ on_message/on_message.py
@@ -14,7 +14,7 @@ class OnMessage(commands.Cog):
         
         if (message.content.lower().startswith("first")):
             await message.channel.send('Second!')
-        elif 'help' in message.content.lower():
+        elif message.content.lower().find("help"):
                 await message.channel.send('Read <#660219603212959747> !')
 
 def setup(bot):

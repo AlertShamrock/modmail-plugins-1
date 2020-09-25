70b32ba52f88ed12e86f6877d06243c41c609766
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-25 17:43:27 -0400

    Update on_message.py

--- on_message/on_message.py
+++ on_message/on_message.py
@@ -16,6 +16,8 @@ class OnMessage(commands.Cog):
             await message.channel.send('Second!')
         elif ('help' in message.content.lower()):
                 await message.channel.send('Read <#660219603212959747> !')
+        elif ('@ShamrockBot' in message.content.lower()):
+                await message.channel.send('Read <#660219603212959747> !')
 
 def setup(bot):
     bot.add_cog(OnMessage(bot))

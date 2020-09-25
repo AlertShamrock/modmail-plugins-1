90d3cdf60e3d1e9de5ba0d9e545bd1535462d3b5
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-25 17:56:08 -0400

    Update on_message.py

--- on_message/on_message.py
+++ on_message/on_message.py
@@ -16,7 +16,7 @@ class OnMessage(commands.Cog):
             await message.channel.send('Second!')
         elif ('help' in message.content.lower()):
                 await message.channel.send('Read <#660219603212959747> !')
-        elif client.user.mention:
+        elif client.user.mention == self:
                 await message.channel.send('Read <#660219603212959747> !')
 
 def setup(bot):

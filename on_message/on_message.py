8e64a37c6b9d727115e370136ca953831d20c6c5
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-25 18:16:53 -0400

    Update on_message.py

--- on_message/on_message.py
+++ on_message/on_message.py
@@ -16,8 +16,8 @@ class OnMessage(commands.Cog):
             await message.channel.send('Second!')
         elif ('help' in message.content.lower()):
                 await message.channel.send('Read <#660219603212959747> !')
-        elif ('shamrock' in message.content.lower()):
-                await message.channel.send('Read <#660219603212959747> !')
+        elif ('discord.gg' in message.content.lower()):
+                message.delete()
 
 def setup(bot):
     bot.add_cog(OnMessage(bot))

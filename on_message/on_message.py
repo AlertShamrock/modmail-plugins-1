87fd4f9c34170033561ff188ed78566b031a55ae
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-09-27 14:18:11 -0400

    Update on_message.py

--- on_message/on_message.py
+++ on_message/on_message.py
@@ -16,7 +16,7 @@ class OnMessage(commands.Cog):
             await message.channel.send('Second!')
         elif ('help' in message.content.lower()):
                 await message.channel.send('Read <#660219603212959747> !')
-        elif message.content.lower().startswith("test"):
+        elif ('test' in message.content.lower()):
                 message = await message.channel.send("hello")
                 await message.edit("newcontent") 
 

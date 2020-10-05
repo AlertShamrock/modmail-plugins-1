7ae26a78066131bcf0c1e752bd9ac8fe786a5962
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-10-05 11:36:09 -0400

    Update lockdown.py

--- lockdown/lockdown.py
+++ lockdown/lockdown.py
@@ -15,54 +15,9 @@ class Lockdown:
     async def lockdown(self, ctx):
        """Lock message sending in the channel. Staff only."""
        try:
-            perms = ctx.channel.overwrites_for(ctx.guild.default_role)
-            if overwrites_everyone.send_messages == False:
-                await self.bot.say("🔒 Channel is already locked down. Use `.unlock` to unlock.")
-                return
-    
-        await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=perms.VIEW_CHANNEL=False)
-            await self.bot.say("🔒 Channel locked down. Only staff members may speak. Do not bring the topic to other channels or risk disciplinary actions.")
-            msg = "🔒 **Lockdown**: {0} by {1} | {2}#{3}".format(ctx.message.channel.mention, ctx.message.author.mention, ctx.message.author.name, ctx.message.author.discriminator)
-            await self.bot.send_message(self.bot.modlogs_channel, msg)
-       except discord.errors.Forbidden:
-            await self.bot.say("💢 I don't have permission to do this.")
-
-    @commands.has_permissions(manage_messages=True)
-    @commands.command(pass_context=True, name="softlock")
-    async def softlock(self, ctx):
-       """Lock message sending in the channel, without the "disciplinary action" note. Staff only."""
-       try:
-            overwrites_everyone = ctx.message.channel.overwrites_for(ctx.guild.default_role)
-            if overwrites_everyone.send_messages == False:
-                await self.bot.say("🔒 Channel is already locked down. Use `.unlock` to unlock.")
-                return
-            overwrites_everyone.send_messages = False
-            await self.bot.edit_channel_permissions(ctx.message.channel, ctx.guild.default_role, overwrites_everyone)
-            await self.bot.say("🔒 Channel locked.")
-            msg = "🔒 **Soft-lock**: {0} by {1} | {2}#{3}".format(ctx.message.channel.mention, ctx.message.author.mention, ctx.message.author.name, ctx.message.author.discriminator)
-            await self.bot.send_message(self.bot.modlogs_channel, msg)
-       except discord.errors.Forbidden:
-            await self.bot.say("💢 I don't have permission to do this.")
-
-    @commands.has_permissions(manage_messages=True)
-    @commands.command(pass_context=True, name="unlock")
-    async def unlock(self, ctx):
-       """Unlock message sending in the channel. Staff only."""
-       try:
-            overwrites_everyone = ctx.message.channel.overwrites_for(self.bot.everyone_role)
-            overwrites_staff = ctx.message.channel.overwrites_for(self.bot.staff_role)
-            if overwrites_everyone.send_messages == None:
-                await self.bot.say("🔓 Channel is already unlocked.")
-                return
-            overwrites_everyone.send_messages = None
-            overwrites_staff.send_messages = True
-            await self.bot.edit_channel_permissions(ctx.message.channel, self.bot.everyone_role, overwrites_everyone)
-            await self.bot.edit_channel_permissions(ctx.message.channel, self.bot.staff_role, overwrites_staff)
-            await self.bot.say("🔓 Channel unlocked.")
-            msg = "🔓 **Unlock**: {0} by {1} | {2}#{3}".format(ctx.message.channel.mention, ctx.message.author.mention, ctx.message.author.name, ctx.message.author.discriminator)
-            await self.bot.send_message(self.bot.modlogs_channel, msg)
-       except discord.errors.Forbidden:
-            await self.bot.say("💢 I don't have permission to do this.")
-
+          perms = ctx.channel.overwrites_for(ctx.guild.default_role)
+        perms.send_messages=False
+        await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=perms)
+   
 def setup(bot):
     bot.add_cog(Lockdown(bot))
